c3a424fc65b0bcd481af6c700247b4295d182d62
zeildconic <1053375407@qq.com>
2021-03-19 18:51:46 +0800

    框架

--- src/main/java/com/rust/Controller/HomeController.java
+++ src/main/java/com/rust/Controller/HomeController.java
@@ -6,10 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.rust.Item.Testitem;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class HomeController {
     @GetMapping("/test")
     public String testOne(){
-        return JSONObject.toJSONString(new Testitem(1,"hello world!"));
+        ArrayList<Testitem> list = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            list.add(new Testitem(i,"just do it!"));
+        }
+        return JSONObject.toJSONString(list);
     }
 }
